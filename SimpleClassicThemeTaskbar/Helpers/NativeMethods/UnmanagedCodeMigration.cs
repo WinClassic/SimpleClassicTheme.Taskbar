12cb28b265e74668ad71bd07c2c5537e79591217
AEAEAEAE4343 <anis1errais@live.com>
2021-05-17 11:37:18 +0200

    Started porting unmanaged code to C#

---
+++ SimpleClassicThemeTaskbar/Helpers/NativeMethods/UnmanagedCodeMigration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassicThemeTaskbar.Helpers.NativeMethods
+{
+	/// <summary>
+	/// This class is intended for the functions currently present in SimpleClassicThemeTaskbar.UnmanagedCode
+	/// We'll migrate everything here first, and then put it in the appropriate areas of SCTT accordingly.
+	/// Don't move or rename anything out of here before UnmanagedCode is fully migrated.
+	/// </summary>
+	static class UnmanagedCodeMigration
+	{
+		#region NativeImports
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool SystemParametersInfo(SPI uiAction, uint uiParam, ref RECT pvParam, SPIF fWinIni);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+		#endregion
+
+		#region NativeEnums
+		enum SPI : uint
+		{
+			SPI_SETWORKAREA = 0x002F
+		}
+
+		enum SPIF : uint
+		{
+			None = 0x00,
+			SPIF_UPDATEINIFILE = 0x01,
+			SPIF_SENDCHANGE = 0x02,
+			SPIF_SENDWININICHANGE = 0x02
+		}
+		#endregion
+
+		#region NativeStructures
+		[Serializable]
+		[StructLayout(LayoutKind.Sequential)]
+		struct POINT
+		{
+			int X;
+			int Y;
+		}
+
+		[Serializable]
+		[StructLayout(LayoutKind.Sequential)]
+	    struct WINDOWPLACEMENT
+		{
+			public int Length;
+			public int Flags;
+			public int ShowCmd;
+			public POINT MinPosition;
+			public POINT MaxPosition;
+			public RECT NormalPosition;
+		}
+		#endregion
+
+		static void SetWorkingArea(RECT rect, bool sendChange, List<IntPtr> windows)
+		{
+			const int SC_MAXIMIZE = 0xF030;
+			const int SC_RESTORE = 0xF220;
+			const int SW_SHOWMAXIMIZED = 3;
+			const int WM_SYSCOMMAND = 0x0112;
+
+			if (!SystemParametersInfo(SPI.SPI_SETWORKAREA, 0, ref rect, sendChange ? SPIF.SPIF_SENDWININICHANGE | SPIF.SPIF_UPDATEINIFILE : 0))
+			{
+				int errorCode = Marshal.GetLastWin32Error();
+				Logger.Log(LoggerVerbosity.Basic, "UnmanagedCode/SetWorkArea", $"Could not set the working area. {errorCode}");
+				throw new Win32Exception(errorCode);
+			}
+
+			foreach (IntPtr ptr in windows)
+			{
+				WINDOWPLACEMENT windowPlacement = new WINDOWPLACEMENT();
+				windowPlacement.Length = Marshal.SizeOf<WINDOWPLACEMENT>();
+				GetWindowPlacement(ptr, ref windowPlacement);
+				if (windowPlacement.ShowCmd == SW_SHOWMAXIMIZED)
+				{
+					User32.SendMessage(ptr, WM_SYSCOMMAND, SC_RESTORE, 0);
+					User32.SendMessage(ptr, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
+				}
+			}
+		}
+	}
+}
