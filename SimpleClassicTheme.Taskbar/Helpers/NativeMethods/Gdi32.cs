dc3ec220afcb8a82641a70037ed8a499433b6700
Craftplacer <craftplacer@craftplacer.moe>
2021-08-06 19:06:26 +0200

    Unset `SetLastError` on gdi32.dll `SelectObject`

--- SimpleClassicTheme.Taskbar/Helpers/NativeMethods/Gdi32.cs
+++ SimpleClassicTheme.Taskbar/Helpers/NativeMethods/Gdi32.cs
@@ -8,7 +8,7 @@ namespace SimpleClassicTheme.Taskbar.Helpers.NativeMethods
         [DllImport(nameof(Gdi32))]
         internal static extern void SetTextColor(IntPtr hdc, uint color);
 
-        [DllImport(nameof(Gdi32))]
+        [DllImport(nameof(Gdi32), SetLastError = false)]
         internal static extern IntPtr SelectObject(IntPtr hdc, IntPtr h);
     }
 }
