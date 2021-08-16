f943cbd15e28440c0e6f13707600069a67ea2e16
Craftplacer <craftplacer@craftplacer.moe>
2021-08-16 17:36:21 +0200

    Add `Provider` and `ShellHookWindowProvider`

---
+++ SimpleClassicTheme.Taskbar/Providers/ShellHookWindowProvider.cs
@@ -0,0 +1,29 @@
+﻿using SimpleClassicTheme.Taskbar.Helpers;
+using SimpleClassicTheme.Taskbar.Native;
+
+using System;
+
+namespace SimpleClassicTheme.Taskbar.Providers
+{
+    internal class ShellHookWindowProvider : Provider<Window>
+    {
+        public ShellHook ShellHook { get; private set; }
+
+        public ShellHookWindowProvider(ShellHook shellHook)
+        {
+            ShellHook = shellHook ?? throw new ArgumentNullException(nameof(shellHook));
+            ShellHook.WindowCreated += ShellHook_WindowCreated;
+            ShellHook.WindowDestroyed += ShellHook_WindowDestroyed;
+        }
+
+        private void ShellHook_WindowCreated(object sender, IntPtr e)
+        {
+            OnItemAdded(new(e));
+        }
+
+        private void ShellHook_WindowDestroyed(object sender, IntPtr e)
+        {
+            OnItemRemoved(new(e));
+        }
+    }
+}
